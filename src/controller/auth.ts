d457da30c570e40ae02d9fb11ff4f5072a00e80c
Tugsbileg-Enkhtaivan <tugsuindawork@gmail.com>
2025-04-11 06:36:50 +0800

    good

--- src/controller/auth.ts
+++ src/controller/auth.ts
@@ -25,7 +25,6 @@ export const register = async (req, res) => {
 
 export const login = async (req, res) => {
   const { email, password } = req.body;
-
   const user = await User.findOne({ email: email });
 
   if (!user) {
