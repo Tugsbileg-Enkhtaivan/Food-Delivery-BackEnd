1508d96ab24a8e365ac967e6837c1b4bf12654b0
Tugsbileg-Enkhtaivan <tugsuindawork@gmail.com>
2025-04-10 11:53:30 +0800

    fix next config and cloudinary

--- src/utils/connection.ts
+++ src/utils/connection.ts
@@ -1,8 +1,7 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
-
 
 const MONGODB_URI = process.env.MONGODB_URI;
 export const connection = async () => {
