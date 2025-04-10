9f5df21d027b9f860902ef1a6c981dede2973cec
Tugsbileg-Enkhtaivan <tugsuindawork@gmail.com>
2025-04-10 09:54:41 +0800

    zz

--- src/controller/food.ts
+++ src/controller/food.ts
@@ -79,17 +79,24 @@ export const patchUpdateFoods = async (request, response) => {
 };
 
 export const deleteFood = async (request, response) => {
-    const { id } = request.params;
+  const { id } = request.params;
 
-    try {
-        const deletedFood = await Food.findByIdAndDelete(id);
+  try {
+    const deletedFood = await Food.findByIdAndDelete(id);
 
-        if (!deletedFood) {
-            return response.status(404).json({ success: false, message: "Food item not found" })
-        }
-        return response.json({ success: true, message: "Food item deleted successfully" });
-    } catch (error) {
-        console.log(error);
-        return response.status(500).json({ sucess: false, message: "Server error" });
+    if (!deletedFood) {
+      return response
+        .status(404)
+        .json({ success: false, message: "Food item not found" });
     }
-}
+    return response.json({
+      success: true,
+      message: "Food item deleted successfully",
+    });
+  } catch (error) {
+    console.log(error);
+    return response
+      .status(500)
+      .json({ success: false, message: "Server error" });
+  }
+};
