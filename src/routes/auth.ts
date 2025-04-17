1819be8e5dc2d2048fddc2cba7e52fd922e7ff41
Tugsbileg-Enkhtaivan <tugsuindawork@gmail.com>
2025-04-17 12:03:13 +0800

    auth

--- src/routes/auth.ts
+++ src/routes/auth.ts
@@ -1,8 +1,13 @@
 import { Router } from "express";
 import { login, register } from "../controller/auth";
+import { resetRequest, updatePassword } from "../controller/reset-password";
 
 const authRouter = Router();
 
-authRouter.post("/register", register).post("/login", login);
+authRouter
+.post("/register", register)
+.post("/login", login)
+.post("/reset-password", resetRequest)
+.post("/update-password", updatePassword)
 
 export { authRouter };
